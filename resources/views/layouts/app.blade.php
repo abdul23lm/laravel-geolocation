1a89dec177f5701e44a3ae710fd3aa1735231c7b
abdul23lm <abdul23lm@gmail.com>
2020-04-26 08:16:29 +0700

    Here Maps Installation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,12 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js"></script>
+    <script src="https://js.api.here.com/v3/3.1/mapsjs-core.js" type="text/javascript" charset="utf-8"></script>
+    <script src="https://js.api.here.com/v3/3.1/mapsjs-service.js" type="text/javascript" charset="utf-8"></script>
+    <script src="https://js.api.here.com/v3/3.1/mapsjs-ui.js" type="text/javascript" charset="utf-8"></script>
+    <link rel="stylesheet" type="text/css" href="https://js.api.here.com/v3/3.1/mapsjs-ui.css" />
+    <script src="https://js.api.here.com/v3/3.1/mapsjs-mapevents.js" type="text/javascript" charset="utf-8"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -36,7 +40,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
-                            <a href="{{ route('space.index')}}" class="nav-link">Space</a>
+                            <a href="{{ route('space.index') }}" class="nav-link">Space</a>
                         </li>
                     </ul>
 
@@ -80,7 +84,12 @@
             @yield('content')
         </main>
     </div>
+
     <script src="{{ asset('js/app.js') }}"></script>
     @stack('script')
+    <script>
+        window.hereApiKey = "{{ env('HERE_API_KEY') }}"
+    </script>
+    <script src="{{ asset('js/here.js') }}"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('space', 'SpaceController');
+Route::get('/browse/space', 'SpaceController@browse')->name('space.browse');
