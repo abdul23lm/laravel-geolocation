4b3c7ec0462336e5f58046966a7e143221652007
Abdul Latif Munjiat <58551842+abdul23lm@users.noreply.github.com>
2020-07-22 22:03:21 +0700

    Update create.blade.php

--- resources/views/pages/space/create.blade.php
+++ resources/views/pages/space/create.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <x-navigation></x-navigation>
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header">Submit My Space</div>
 
