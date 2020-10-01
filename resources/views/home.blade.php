03902975206095956a25961bb18d808ecac09ef7
Abdul Latif Munjiat <58551842+abdul23lm@users.noreply.github.com>
2020-10-01 23:08:41 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,11 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
                     You are logged in!
                 </div>
