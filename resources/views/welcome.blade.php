f858d54369fc78f82953e6c907c1dc64049850e8
Abdul Latif Munjiat <58551842+abdul23lm@users.noreply.github.com>
2020-10-08 23:35:05 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
                 background-color: #fff;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
-                font-weight: 300;
+                font-weight: 200;
                 height: 100vh;
                 margin: 0;
             }
